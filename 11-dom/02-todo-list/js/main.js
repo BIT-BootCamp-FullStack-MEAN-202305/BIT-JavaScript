637bdc8fc89251bd90010bc3da12c2822384295d
Juan Carlos Jimenez Gutierrez <jcjimenez29@misena.edu.co>
2023-06-14 20:16:23 -0500

    [+] DOM: -R-- TodoList (localStorage)

--- 11-dom/02-todo-list/js/main.js
+++ 11-dom/02-todo-list/js/main.js
@@ -2,6 +2,8 @@ const
     $form = document.querySelector( '[data-form]' ),
     $showDataEl = document.querySelector( '.show-data' );
 
+// console.log( $showDataEl );
+
 show();
 
 // Tarea: Obtener los datos de localStorage
@@ -16,6 +18,42 @@ function add() {}
 function show() {
     const data = getData();
     console.log( data );
+
+    const
+        $tableEl = document.createElement( 'table' ), 
+        $tbodyEl = document.createElement( 'tbody' );
+
+    $tableEl.setAttribute( 'border', '1' );
+    $tableEl.setAttribute( 'cellspacing', '1px' );
+    $tableEl.setAttribute( 'cellpadding', '1px' );
+    
+
+    data.forEach( function( todo ) {
+        const 
+            $trEl = document.createElement( 'tr' ),
+            $tdIdEl = document.createElement( 'td' ),
+            $tdNombreEl = document.createElement( 'td' ),
+            $tdPrecioEl = document.createElement( 'td' );
+
+        $tdIdEl.innerText = todo.id;
+        $tdNombreEl.innerText = todo.product;
+        $tdPrecioEl.innerText = todo.price;
+
+        $trEl.appendChild( $tdIdEl );
+        $trEl.appendChild( $tdNombreEl );
+        $trEl.appendChild( $tdPrecioEl );
+        $tbodyEl.appendChild( $trEl );
+        
+        // $showDataEl.appendChild( $tbodyEl );
+
+        console.log( $trEl );        
+
+        // $showDataEl.appendChild()
+        console.log( todo );
+    });
+
+    $tableEl.appendChild( $tbodyEl );
+    $showDataEl.appendChild( $tableEl );
 }
 
 // Update
@@ -25,8 +63,8 @@ function update() {}
 function remove() {}
 
 
-console.log( $form );
-console.log( $showDataEl );
+// console.log( $form );
+// console.log( $showDataEl );
 
 $form.addEventListener( 'submit', ( e ) => {
     e.preventDefault();
