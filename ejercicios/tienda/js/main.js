ee1904cbec67b2119c90fa515341810b232d4a0d
Juan Carlos Jimenez Gutierrez <jcjimenez29@misena.edu.co>
2023-06-20 21:12:28 -0500

    [+] Ejercicio (Tienda): David, Christian

--- ejercicios/tienda/js/main.js
+++ ejercicios/tienda/js/main.js
@@ -16,7 +16,53 @@ class Product {
     }
 }
 
+class Section {
+    // Atributos
+    #name;
+    #products = [];
+
+    constructor( section = 'unclassified' ) {
+        this.#name = section;
+    }
+
+    addProduct( newProduct ) {
+        this.#products.push( newProduct );
+    }
+}
+
+class Inventory {
+    // Atributos
+    #sectionList;
+
+    addSection( newSection ) {
+        this.#sectionList.push( newSection );
+    }
+}
+
+/** Pruebas */
+// const seccionVerduras = new Section( 'Coles' );
+// console.log( seccionVerduras );
+
+// const coliflor = new Product( 'Coliflor', 34, 3 );
+
+// seccionVerduras.addProduct( coliflor );
+// seccionVerduras.addProduct( new Product( 'Brocoli', 12, 5 ) );
+// console.log( seccionVerduras );
+
+// const sectionUnclassified = new Section();
+// sectionUnclassified.addProduct( new Product( 'Papa', 30, 1.5 ) );
+
+// console.log( sectionUnclassified );
+
 /** Implementacion */
+function capturaSeccion() {
+    const nombreSeccion = prompt( 'Digite nombre de la seccion para almacenar en inventario' );
+
+    const nuevaSeccion = new Section( nombreSeccion );
+    // console.log( nuevaSeccion );
+
+    return nuevaSeccion;
+}
 
 function capturaProducto() {
     const
@@ -25,19 +71,30 @@ function capturaProducto() {
         precio   = Number( prompt( `Precio unitario de ${ nombre }` ) );
 
     // Asignamos a una constante la crecion de un objeto a partir de la clase Producto
-    const telefono = new Product( nombre, cantidad, precio );
-    console.log( telefono );
+    const nuevoProducto = new Product( nombre, cantidad, precio );
+    // console.log( nuevoProducto );
+
+    return nuevoProducto;
 }
 
-// do { sentencias } while( condidion );
+function init() {
+    let option;
+    const seccion = capturaSeccion();
+    
+    // do { sentencias } while( condidion );
+    do {
+        const producto = capturaProducto();
+    
+        seccion.addProduct( producto );
+    
+        option = prompt( 'Desea continuar, S/N' );
+    } while( option.toLowerCase() == 's' );
+    
+    console.log( seccion );
+}
 
-let option;
+// init();
 
-do {
-    capturaProducto();
-
-    option = prompt( 'Desea continuar, S/N' );
-} while( option.toLowerCase() == 's' );
 
 
 
